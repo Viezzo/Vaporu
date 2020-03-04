e08a700b41c30eeb88a27b2bdbc9e8f33531e228
Adrian Traviezo <aft2115@columbia.edu>
2020-03-04 14:33:59 -0500

    Reverted Script Importer to CSV version, which is in scriptImporter_old.js. Also added notification popup for when there's a new version available. The new version MUST be added to Vaporu/version.text to reflect the notification.

--- Vaporu/lib/checkForUpdates.js
+++ Vaporu/lib/checkForUpdates.js
@@ -9,11 +9,20 @@ $(document).ready(function() {
             dataType: 'text',
             success: function(data){
                 var thisVersion = $("#panelVersion").text();
+                $("#updateWarning").text("Vaporu version " + data + " is available to install using Self Service.")
                 if (parseFloat(thisVersion) != parseFloat(data))
                     $("#updateNotification").css("display", "inline-block");
             }
         });
     }
 
-    $('[data-toggle="updateTooltip"]').tooltip();
+    $(".fa-exclamation-circle").hover(function () {
+        $(this).animate({color: "red"});
+        $("#exporterRefreshTooltip").css("visibility", "visible");
+        $("#updateWarning").slideDown();
+    }, function(){
+        $(this).animate({color: "#ff4000"});
+        $("#exporterRefreshTooltip").css("visibility", "hidden");
+        $("#updateWarning").slideUp();
+    });
 });
